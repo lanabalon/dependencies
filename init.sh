9507c4600342f40cadf1348de4980e139aef80b1
Luis Anabalon <lanabalon@falabella.cl>
2019-04-25 14:06:40 -0400

    ci: change for logging and runner installation

--- init.sh
+++ init.sh
@@ -5,11 +5,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get update
-echo "Installing Docker-ce" >> /tmp/runner-$(date '+\%Y\%m\%d').log
+echo "Installing Docker-ce" >> /tmp/runner-$(date '+%Y%m%d').log
 sudo apt-get -y install docker-ce
-echo "Installing Ansible" >> /tmp/runner-$(date '+\%Y\%m\%d').log
+echo "Installing Ansible" >> /tmp/runner-$(date '+%Y%m%d').log
 sudo apt-get install ansible -y
-
 
 GITLAB_URL_RUNNER_PKG="https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh"
 HOST=`hostname`;
@@ -23,44 +22,48 @@ then
 	GITLAB_URL="https://fala.cl"
 fi
 
+#Installing gitlab-runner 
+echo "Installing runner on the VM provisioned"
+curl -L ${GITLAB_URL_RUNNER_PKG} | sudo bash
+sudo apt-get -y install gitlab-runner
+
 #Unregistering gitlab-runner 
-echo "Unregistering gitlab-runner" >> /tmp/runner-$(date '+\%Y\%m\%d').log
+echo "Unregistering gitlab-runner" >> /tmp/runner-$(date '+%Y%m%d').log
 sudo gitlab-runner unregister --all-runners
 
 registerAsDocker(){
-	echo "Registering runner as expected DOCKER executor ${GITLAB_URL} ${GITLAB_TOKEN_RUNNER}" 
-	sudo gitlab-runner register \
-	  --non-interactive \
-	  --name "gitlab-runner-${HOST}" \
-	  --url "${GITLAB_URL}/" \
-	  --registration-token "${GITLAB_TOKEN_RUNNER}" \
-	  --executor 'docker' \
-	  --run-untagged="true" \
-	  --locked="false" \
-	  --docker-privileged \
-	  --docker-image 'alpine:3.7'
+        echo "Registering runner as expected DOCKER executor ${GITLAB_URL} ${GITLAB_TOKEN_RUNNER}" 
+        sudo gitlab-runner register \
+          --non-interactive \
+          --name "gitlab-runner-${HOST}" \
+          --url "${GITLAB_URL}/" \
+          --registration-token "${GITLAB_TOKEN_RUNNER}" \
+          --executor 'docker' \
+          --run-untagged="true" \
+          --locked="false" \
+          --docker-privileged \
+          --docker-image 'alpine:3.7'
 }
 
 registerAsShell(){
-echo "Registering runner as default Shell executor ${GITLAB_URL} ${GITLAB_TOKEN_RUNNER}" >>/tmp/runner-$(date '+\%Y\%m\%d').log
-	sudo gitlab-runner register \
-	  --non-interactive \
-	  --name "gitlab-runner-${HOST}" \
-	  --url "${GITLAB_URL}/" \
-	  --registration-token "${GITLAB_TOKEN_RUNNER}" \
-	  --executor "shell" \
-	  --run-untagged="true" \
-	  --locked="false"
+echo "Registering runner as default Shell executor ${GITLAB_URL} ${GITLAB_TOKEN_RUNNER}" >>/tmp/runner-$(date '+%Y%m%d').log
+        sudo gitlab-runner register \
+          --non-interactive \
+          --name "gitlab-runner-${HOST}" \
+          --url "${GITLAB_URL}/" \
+          --registration-token "${GITLAB_TOKEN_RUNNER}" \
+          --executor "shell" \
+          --run-untagged="true" \
+          --locked="false"
 }
 
 if [ "x${GITLAB_RUNNER_EXECUTOR}" != "x" ];
 then
-	registerAsDocker;
-else 
-	registerAsShell;
+        registerAsDocker;
+else
+        registerAsShell;
 fi
 
 #Restarting gitlab-runner
-echo "Restarting gitlab-runner" >> /tmp/runner-$(date '+\%Y\%m\%d').log
+echo "Restarting gitlab-runner" >> /tmp/runner-$(date '+%Y%m%d').log
 sudo gitlab-runner restart
-
